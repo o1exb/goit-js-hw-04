5a38ece42482aefbfbdf1f021210b9e6eefb5cb7
jt <olex.bro92@gmail.com>
2025-02-14 14:18:37 +0100

    js-hw4-correction1

--- js/task-1.js
+++ js/task-1.js
@@ -3,7 +3,7 @@ function isEnoughCapacity(products, containerSize) {
   for (const product in products) {
     total += products[product];
   }
-  return total < containerSize;
+  return total <= containerSize;
 }
 
 console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
